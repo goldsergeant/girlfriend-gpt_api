0ca31bcdc5d39e21ec90cc8717d64894256df7a2
goldsergeant <rmagksfla000@gmail.com>
2023-08-02 18:02:46 +0900

    chore: Add django-cors-headers

--- girlfriend_gpt/settings.py
+++ girlfriend_gpt/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'account.apps.AccountConfig',
+    'corsheaders',
 
     'allauth',
     'allauth.account',
@@ -97,6 +98,7 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -107,6 +109,31 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'girlfriend_gpt.urls'
+
+CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOW_CREDENTIALS = True
+
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
 
 TEMPLATES = [
     {
