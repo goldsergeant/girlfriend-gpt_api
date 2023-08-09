bdc0fdf393eb5996f52af0b24edea92f6733b896
goldsergeant <rmagksfla000@gmail.com>
2023-08-09 18:13:38 +0900

    feat: create Charles and Mika

--- chat/models.py
+++ chat/models.py
@@ -11,7 +11,11 @@ User = get_user_model()
 class Character(models.Model):
     name=models.CharField(max_length=100)
     system=models.TextField()
-
+    GENDERS = (
+        ('M', '남성(Man)'),
+        ('W', '여성(Woman)'),
+    )
+    gender = models.CharField(verbose_name='성별', max_length=1, choices=GENDERS)
 class Message(models.Model):
     from_user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="messages_from_me"
