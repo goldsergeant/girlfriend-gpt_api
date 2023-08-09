e4a759cdd648f856a5a08f20f03cd3a9e0106543
goldsergeant <rmagksfla000@gmail.com>
2023-08-09 17:42:58 +0900

    feat: create Character

--- chat/admin.py
+++ chat/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from chat.models import Message
+from chat.models import Message, Character
 
 # Register your models here.
-admin.site.register(Message)
+admin.site.register(Character)
